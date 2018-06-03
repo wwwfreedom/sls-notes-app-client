0102c7a2a25d4b01a3de0962212f7df30a17006f
Kevin <tig3r86@gmail.com>
2018-06-03 22:22:53 +1000

    add logic to load the user session in app state from aws amplify

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component, Fragment } from "react"
+import { Auth } from "aws-amplify"
 import { LinkContainer } from "react-router-bootstrap"
 import { Link } from "react-router-dom"
 import { Nav, Navbar, NavItem } from "react-bootstrap"
@@ -11,7 +12,23 @@ class App extends Component {
 
     this.state = {
       isAuthenticated: false,
+      isAuthenticating: true,
     }
+  }
+
+  //  check if a session object is returned. If so, then it updates the isAuthenticating flag once the process is complete. Also, the Auth.currentSession() method throws an error No current user if nobody is currently logged in. We don’t want to show this error to users when they load up our app and are not signed in
+  async componentDidMount() {
+    try {
+      if (await Auth.currentSession()) {
+        this.userHasAuthenticated(true)
+      }
+    } catch (e) {
+      if (e !== "No current user") {
+        alert(e)
+      }
+    }
+
+    this.setState({ isAuthenticating: false })
   }
 
   userHasAuthenticated = authenticated => {
@@ -23,39 +40,43 @@ class App extends Component {
   }
 
   render() {
+    const { isAuthenticated, isAuthenticating } = this.state
     const childProps = {
-      isAuthenticated: this.state.isAuthenticated,
+      isAuthenticated,
       userHasAuthenticated: this.userHasAuthenticated,
     }
 
     return (
-      <div className="App container">
-        <Navbar fluid collapseOnSelect>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <Link to="/">Scratch</Link>
-            </Navbar.Brand>
-            <Navbar.Toggle />
-          </Navbar.Header>
-          <Navbar.Collapse>
-            <Nav pullRight>
-              {this.state.isAuthenticated ? (
-                <NavItem onClick={this.handleLogout}>Logout</NavItem>
-              ) : (
-                <Fragment>
-                  <LinkContainer to="/signup">
-                    <NavItem>Signup</NavItem>
-                  </LinkContainer>
-                  <LinkContainer to="/login">
-                    <NavItem>Login</NavItem>
-                  </LinkContainer>
-                </Fragment>
-              )}
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-        <Routes childProps={childProps} />
-      </div>
+      /* loading user session is an asynchornous process so only render when the is authenticating is done */
+      isAuthenticating === false && (
+        <div className="App container">
+          <Navbar fluid collapseOnSelect>
+            <Navbar.Header>
+              <Navbar.Brand>
+                <Link to="/">Scratch</Link>
+              </Navbar.Brand>
+              <Navbar.Toggle />
+            </Navbar.Header>
+            <Navbar.Collapse>
+              <Nav pullRight>
+                {isAuthenticated ? (
+                  <NavItem onClick={this.handleLogout}>Logout</NavItem>
+                ) : (
+                  <Fragment>
+                    <LinkContainer to="/signup">
+                      <NavItem>Signup</NavItem>
+                    </LinkContainer>
+                    <LinkContainer to="/login">
+                      <NavItem>Login</NavItem>
+                    </LinkContainer>
+                  </Fragment>
+                )}
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+          <Routes childProps={childProps} />
+        </div>
+      )
     )
   }
 }
