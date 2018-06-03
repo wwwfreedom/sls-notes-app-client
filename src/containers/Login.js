078c0173cc46aab84c8d31d591e36d5734706a25
Kevin <tig3r86@gmail.com>
2018-06-03 22:43:51 +1000

    add loader button to improve ux experience during signin

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react"
 import { Auth } from "aws-amplify"
-import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap"
+import { FormGroup, FormControl, ControlLabel } from "react-bootstrap"
+import LoaderButton from "../components/LoaderButton.js"
 import "./Login.css"
 
 export default class Login extends Component {
@@ -8,6 +9,7 @@ export default class Login extends Component {
     super(props)
 
     this.state = {
+      isLoading: false,
       email: "",
       password: "",
     }
@@ -26,6 +28,8 @@ export default class Login extends Component {
   handleSubmit = async event => {
     event.preventDefault()
 
+    this.setState({ isLoading: true })
+
     try {
       await Auth.signIn(this.state.email, this.state.password)
       this.props.userHasAuthenticated(true)
@@ -33,6 +37,7 @@ export default class Login extends Component {
       this.props.history.push("/")
     } catch (e) {
       alert(e.message)
+      this.setState({ isLoading: false })
     }
   }
 
@@ -57,14 +62,15 @@ export default class Login extends Component {
               type="password"
             />
           </FormGroup>
-          <Button
+          <LoaderButton
             block
             bsSize="large"
             disabled={!this.validateForm()}
             type="submit"
-          >
-            Login
-          </Button>
+            isLoading={this.state.isLoading}
+            text="Login"
+            loadingText="Logging in..."
+          />
         </form>
       </div>
     )
