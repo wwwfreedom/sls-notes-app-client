cfd5e42dce2ccd0c83c013fba82c0c361a6d940d
Kevin <tig3r86@gmail.com>
2018-06-03 12:06:19 +1000

    add aws configurations

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "ap-southeast-2",
+    BUCKET: "kevin-note-app",
+  },
+  apiGateway: {
+    REGION: "ap-southeast-2",
+    URL: "https://wt80kjnez4.execute-api.ap-southeast-2.amazonaws.com/prod",
+  },
+  cognito: {
+    REGION: "ap-southeast-2",
+    USER_POOL_ID: "ap-southeast-2_H6EQRjo7s",
+    APP_CLIENT_ID: "1lu4vvrtntg10pss0ks8gi7uam",
+    IDENTITY_POOL_ID: "ap-southeast-2:d8b81350-8fd1-44b9-9670-94fa1f7708b9",
+  },
+}
--- src/index.js
+++ src/index.js
@@ -3,7 +3,35 @@ import ReactDOM from "react-dom"
 import { BrowserRouter as Router } from "react-router-dom"
 import App from "./App"
 import registerServiceWorker from "./registerServiceWorker"
+import Amplify from "aws-amplify"
+import config from "./config"
 import "./index.css"
+
+// Amplify refers to Cognito as Auth, S3 as Storage, and API Gateway as API.
+// package use to connect to the aws backend
+Amplify.configure({
+  Auth: {
+    mandatorySignIn: true,
+    region: config.cognito.REGION,
+    userPoolId: config.cognito.USER_POOL_ID,
+    identityPoolId: config.cognito.IDENTITY_POOL_ID,
+    userPoolWebClientId: config.cognito.APP_CLIENT_ID,
+  },
+  Storage: {
+    region: config.s3.REGION,
+    bucket: config.s3.BUCKET,
+    identityPoolId: config.cognito.IDENTITY_POOL_ID,
+  },
+  API: {
+    endpoints: [
+      {
+        name: "notes",
+        endpoint: config.apiGateway.URL,
+        region: config.apiGateway.REGION,
+      },
+    ],
+  },
+})
 
 ReactDOM.render(
   <Router>
