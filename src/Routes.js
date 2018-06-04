944aebc1faadc3f813caafc05ecca6760e1dd20d
Kevin <tig3r86@gmail.com>
2018-06-05 09:23:46 +1000

    replace applied routes with authenticated and unauthenticated route

--- src/Routes.js
+++ src/Routes.js
@@ -8,29 +8,40 @@ import Signup from "./containers/Signup"
 import ConfirmSignup from "./containers/ConfirmSignup"
 import NewNote from "./containers/NewNote.js"
 import Notes from "./containers/Notes"
+import AuthenticatedRoute from "./components/AuthenticatedRoute"
+import UnauthenticatedRoute from "./components/UnauthenticatedRoute"
 
 export default ({ childProps }) => (
   <Switch>
     <AppliedRoute path="/" exact component={Home} props={childProps} />
-    <AppliedRoute path="/login" exact component={Login} props={childProps} />
-    <AppliedRoute path="/signup" exact component={Signup} props={childProps} />
-    <AppliedRoute
+    <UnauthenticatedRoute
+      path="/login"
+      exact
+      component={Login}
+      props={childProps}
+    />
+    <UnauthenticatedRoute
+      path="/signup"
+      exact
+      component={Signup}
+      props={childProps}
+    />
+    <UnauthenticatedRoute
       path="/confirm-signup/:userEmail"
       exact
       component={ConfirmSignup}
       props={childProps}
     />
-    <AppliedRoute
-      path="/notes/:id"
-      exact
-      component={Notes}
-      props={childProps}
-    />
-
-    <AppliedRoute
+    <AuthenticatedRoute
       path="/notes/new"
       exact
       component={NewNote}
+      props={childProps}
+    />
+    <AuthenticatedRoute
+      path="/notes/:id"
+      exact
+      component={Notes}
       props={childProps}
     />
     {/* Finally, catch all unmatched routes */}
